f9636caf021672a31f7be127a4c18f12ee5cd727
Colin <cheinrichs44@gmail.com>
2024-04-07 14:51:13 -0700

    vite builds now

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,15 @@ import { viteCommonjs } from '@originjs/vite-plugin-commonjs'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), viteCommonjs()],
+  plugins: [react()],
+  optimizeDeps: {
+    include: ["mupen64plus-web"]
+  },
+
+  build: {
+    commonjsOptions: {
+      include: [/mupen64plus-web/, /node_modules/]
+    }
+  },
+
 })
