93e471020fd8bb272a1f5b0e13e2eacb477ef8b2
Colin <cheinrichs44@gmail.com>
2024-09-22 17:57:16 -0700

    Revert "run dev server with '--force'; Changes for 'pthread' builds"
    This reverts commit e96b80e6f288326f983169fdd716f105ef8b5068.

--- vite.config.ts
+++ vite.config.ts
@@ -16,10 +16,6 @@ export default defineConfig({
   },
 
   server: {
-    port: 3000,
-    headers: {
-      'Cross-Origin-Embedder-Policy': 'require-corp',
-      'Cross-Origin-Opener-Policy': 'same-origin'
-    }
+    port: 3000
   }
 })
